a06eb20b1cdf6f7ba060d0bd7e050da4d795b736
Dhanush D <dhanushddhanushd668@gmail.com>
2025-07-19 07:14:33 +0530

    new branch

--- src/main/java/com/attendance/util/SmsUtil.java
+++ src/main/java/com/attendance/util/SmsUtil.java
@@ -5,9 +5,9 @@ import com.twilio.rest.api.v2010.account.Message;
 
 public class SmsUtil {
     // Replace with your Twilio Account SID and Auth Token
-    public static final String ACCOUNT_SID = "ACf09596b3d409a312f813335d82298b4c";
-    public static final String AUTH_TOKEN = "aeeff239bd8611497eeda2b52f25cf8a";
-    public static final String FROM_PHONE = "+15734923988"; // e.g., "+1234567890"
+    public static final String ACCOUNT_SID = "################";
+    public static final String AUTH_TOKEN = "#############";
+    public static final String FROM_PHONE = "###############"; // e.g., "+1234567890"
 
     static {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
